523f361d53ad1467e18eb34884e3bc7661cb60e5
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-03-20 10:37:22 +0530

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+    let words = "";
+    if (times < 0) {
+        return "ERROR";
+    }
 
+    for (i = 0; i < times; i++) {
+        words += word;
+    }
+    return words;
 };
 
 // Do not edit below this line
